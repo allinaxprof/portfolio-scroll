b2bd56beb0cd023666efd9f28c114bb925d0ed10
allinaxprof <152559182+allinaxprof@users.noreply.github.com>
2026-01-27 10:35:13 -0500

    add: gh-pages for deployment fix: make correct baseURL

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="https://allinaxprof.github.io/portfolio-scroll">
+    <BrowserRouter basename="">
       <App />
     </BrowserRouter>
   </StrictMode>,
